2fcbabc5e42e7f0cc9fb0e7b3e668925c1ceae84
Craig Palermo <cpalermo2@gmail.com>
2014-09-17 18:47:21 -0700

    added new and create to controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,18 @@ class TodosController < ApplicationController
     def index
         @todos = Todo.all
     end
+
+    def new
+        @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(params[:todo].permit(:title, :done))
+
+        if @todo.save
+            redirect_to "/todos"
+        else
+            render :new
+        end
+    end
 end
